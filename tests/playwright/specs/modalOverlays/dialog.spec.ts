de51f27d24a311cd57f902833fc2b39ebd2d3f63
Cristian Heiu <cristian@gmail.com>
2026-05-25 20:42:11 +0300

    add visual snapshot

--- tests/playwright/specs/modalOverlays/dialog.spec.ts
+++ tests/playwright/specs/modalOverlays/dialog.spec.ts
@@ -1,9 +1,9 @@
-import { expect, test } from '../../fixtures/base_fixture';
+import { test, expect } from '../../fixtures/base_fixture';
 
 test.describe('Dialog page', () => {
   test('visual test for dialog page', async ({ page }) => {
     await page.goto('http://localhost:4200');
 
-    await expect(page).toHaveScreenshot();
+    await expect(page).toHaveScreenshot('dialog-page.png');
   });
 });
